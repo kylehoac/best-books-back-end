0a18475c8df544e4584a2514c4224f832196d3cb
Kyle Hoac <hoackyle@gmail.com>
2021-04-27 15:44:49 -0700

    added schema and model

--- server.js
+++ server.js
@@ -11,9 +11,51 @@ app.use(cors());
 
 const PORT = process.env.PORT
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/books', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
+
+const bookSchema = new mongoose.Schema({
+    name: {type: String},
+    description: {type: String},
+    status: {type: String}
+});
+
+const userSchema = new mongoose.Schema({
+    email: {type: String} ,
+    books: [bookSchema]
+});
+
+const User = mongoose.model('User', userSchema)
+
+const userModel = new User({
+    email: 'potato@potato.com',
+    books: [
+        {name: 'potato',
+        description: 'potato book',
+        status: 'baked'},
+        {name: 'banana',
+        description: 'banana book',
+        status: 'peeled'
+        }
+    ]
+});
+
+userModel.save()
+console.log(userModel);
+// const Books = mongoose.model('Books', userSchema);
+
+
 
 app.get('/',(request, response) => {
     response.send('hi');
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+// Referenced code on mongoosejs.com quick start guide
