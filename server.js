d378354a4ea9bf6bb3d2d2dc25af3104fca31822
Dion Johnson <dionjwa@gmail.com>
2021-04-27 18:14:30 -0700

    updated server

--- server.js
+++ server.js
@@ -1,40 +1,27 @@
 'use strict';
-
+//dependencies
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-
 const app = express();
 app.use(cors());
-
-const PORT = process.env.PORT
-
+const PORT = process.env.PORT || 3003
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+//
+//components
+const User = require('./models/schemas.js');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/books', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
+  console.log('we\'re connected!');
 });
 
-const bookSchema = new mongoose.Schema({
-    name: {type: String},
-    description: {type: String},
-    status: {type: String}
-});
-
-const userSchema = new mongoose.Schema({
-    email: {type: String} ,
-    books: [bookSchema]
-});
-
-const User = mongoose.model('User', userSchema)
-
-const userModel = new User({
-    email: 'potato@potato.com',
+const book1 = new User({
+    email: 'dionjwa@gmail.com',
     books: [
         {name: 'potato',
         description: 'potato book',
@@ -42,20 +29,34 @@ const userModel = new User({
         {name: 'banana',
         description: 'banana book',
         status: 'peeled'
+        },
+        {name: 'bingo',
+        description: 'dawg',
+        status: 'Farm Animal'
         }
     ]
 });
+book1.save();
+// console.log({ book1 });
+//
+//apps
+app.get('/books', getSchemasDB)
+function getSchemasDB(request, response) {
+    const email = request.query.email;
+    console.log(email);
+    User.find({ email }, (err, client) => {
+        if (err) return console.error(err);
+        console.log({ client });
+        response.send(client.length ? client[0].books : 'No books');
+      }) 
+}
 
-userModel.save()
-console.log(userModel);
-// const Books = mongoose.model('Books', userSchema);
-
-
-
-app.get('/',(request, response) => {
+app.get('*', (request, response) => {
+    response.status(400, 404, 500).send('Error: Book not Found');
+});
+//
+//POL
+app.get('/', (request, response) => {
     response.send('hi');
 })
-
-app.listen(PORT, () => console.log(`listening on port ${PORT}`));
-
-// Referenced code on mongoosejs.com quick start guide
+//
